cde68c7eb2ad3cf058cf732cff5c139441c89afb
Daniel Herrero Tomás <dherreto@mercadona.es>
2022-03-17 16:20:16 +0100

    test: Product-detail coverage

--- src/app/pages/main/pages/product-detail/product-detail.component.spec.ts
+++ src/app/pages/main/pages/product-detail/product-detail.component.spec.ts
@@ -1,11 +1,18 @@
 import { CommonModule, Location } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
+import { Product } from 'src/app/models/product';
 import { mockProducts } from 'src/app/models/product.mock';
 import { ProductService } from 'src/app/services/product.service';
 import { ProductDetailComponent } from './product-detail.component';
@@ -16,16 +23,7 @@ describe('ProductDetailComponent', () => {
 
   let route: ActivatedRoute;
   let productService: ProductService;
-  /*const mockedLocation:any = jasmine.createSpyObj<Location>('Location', ['back']);
-  mockedLocation.back.and.returnValue();*/
-  let mockLocationBack: boolean = false;
-  const mockedLocation = {
-    back: () => {
-      mockLocationBack = true;
-    },
-  };
-
-  // let location: Location;
+  let location: Location;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -36,21 +34,13 @@ describe('ProductDetailComponent', () => {
         RouterTestingModule,
         FormsModule,
       ],
-      /* providers: [
-       { provider: Location, useValue: mockedLocation }
-      ]*/
     });
 
     fixture = TestBed.createComponent(ProductDetailComponent);
     component = fixture.componentInstance; // DashboardComponent test instance
     productService = TestBed.inject(ProductService);
     route = TestBed.inject(ActivatedRoute);
-    // location = TestBed.inject(Location); // -> fails
-    component['location'] = mockedLocation as Location;
-  });
-
-  afterEach(() => {
-    mockLocationBack = false;
+    location = TestBed.inject(Location);
   });
 
   it('should exist', () => {
@@ -69,9 +59,19 @@ describe('ProductDetailComponent', () => {
     expect(component.product.id).toBe(11);
   });
 
+  it('should get empty product info if param id is empty', () => {
+    spyOn(route.snapshot.paramMap, 'get').and.returnValue(null);
+    const spyProductService = spyOn(productService, 'getProduct').and.returnValue(of({} as Product));
+
+    fixture.detectChanges();
+
+    expect(spyProductService).toHaveBeenCalledWith(0);
+  });
+
   it('should go back if press goBack button', () => {
     spyOn(route.snapshot.paramMap, 'get').and.returnValue('11');
     spyOn(productService, 'getProduct').and.returnValue(of(mockProducts[0]));
+    const spyLocation = spyOn(location, 'back');
 
     fixture.detectChanges();
 
@@ -79,11 +79,38 @@ describe('ProductDetailComponent', () => {
     btn.nativeElement.click();
 
     fixture.detectChanges();
-
-    // expect(mockedLocation.back).toHaveBeenCalled();
-    expect(mockLocationBack).toBeTrue();
+    expect(spyLocation).toHaveBeenCalled();
   });
 
+  it('should update product changes when press Save button', () => {
+    spyOn(route.snapshot.paramMap, 'get').and.returnValue('11');
+    spyOn(productService, 'getProduct').and.returnValue(of(mockProducts[0]));
+    spyOn(productService, 'updateProduct').and.returnValue(of());
 
+    fixture.detectChanges();
 
+    const input = fixture.debugElement.query(By.css('#quality'));
+    const btn = fixture.debugElement.query(By.css('.btn-primary'));
+
+    input.nativeElement.value = 30;
+    input.nativeElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    btn.nativeElement.click();
+
+    expect(component.product.quality).toBe(30);
+    expect(productService.updateProduct).toHaveBeenCalled();
+  });
+
+  it('should go back after update product', () => {
+    spyOn(productService, 'updateProduct').and.returnValue(of({}));
+    const spyGoBack = spyOn(location, 'back');
+
+    component.product = mockProducts[0];
+    component.product.quality = 22;
+
+    component.save();
+    expect(productService.updateProduct).toHaveBeenCalled();
+    expect(spyGoBack).toHaveBeenCalled();
+  });
 });
