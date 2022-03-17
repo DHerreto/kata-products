73f5e2bc89d04ccd0dafef02b3cdbc32f47aaa87
Daniel Herrero Tomás <dherreto@mercadona.es>
2022-03-17 19:45:48 +0100

    feat: Dashboard full coverage

--- src/app/pages/main/pages/dashboard/dashboard.component.spec.ts
+++ src/app/pages/main/pages/dashboard/dashboard.component.spec.ts
@@ -4,6 +4,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
+import { Product, ProductType } from 'src/app/models/product';
 import { mockProducts } from 'src/app/models/product.mock';
 import { ProductService } from 'src/app/services/product.service';
 import { DashboardComponent } from './dashboard.component';
@@ -27,7 +28,7 @@ describe('DashboardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('should manage products', () => {
+  describe('should retrieve products', () => {
     it('should retrieve products onInit', () => {
       const serviceSpy = spyOn(productService, 'getProducts').and.returnValue(
         of(mockProducts)
@@ -48,6 +49,7 @@ describe('DashboardComponent', () => {
       expect(component.products.length).toBe(cards.length);
     });
   });
+
   describe('Product simulation', () => {
     it('should be accessible through the template', () => {
       // is it a good idea to call the function?
@@ -64,6 +66,121 @@ describe('DashboardComponent', () => {
     });
 
     // TODO: complete functional test
+
+    it("should degrade Q to twice the speed once R is reached", () => {
+      const mockProduct = new Product(11, 'Pan', 0, 10, ProductType.perishable);
+      component.products = [mockProduct];
+
+      component.simulation();
+      expect(component.products[0].quality).toBe(8);
+
+      component.simulation();
+      expect(component.products[0].quality).toBe(6);
+
+    });
+
+    it('should downgrade Q to 1 if it has not yet reached R', () => {
+      const mockProduct = new Product(11, 'Pan', 10, 10, ProductType.perishable);
+      component.products = [mockProduct];
+
+      component.simulation();
+      expect(component.products[0].quality).toBe(9);
+
+      component.simulation();
+      expect(component.products[0].quality).toBe(8);
+    });
+
+    it('should not set Q less than 0', () => {
+      const mockProduct = new Product(11, 'Pan', 0, 1, ProductType.perishable);
+      component.products = [mockProduct];
+
+      component.simulation();
+      component.simulation();
+      component.simulation();
+      component.simulation();
+
+      expect(component.products[0].quality).toBe(0);
+    });
+
+    describe('Products with type vintage', () => {
+      it('should increase Q at 1 if R is up to 0', () => {
+        const mockProduct = new Product(11, 'Chess', 10, 1, ProductType.vintage);
+        component.products = [mockProduct];
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(2);
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(3);
+
+      });
+
+      it('should increase Q at 2 if R is down to 0', () => {
+        const mockProduct = new Product(11, 'Chess', 0, 1, ProductType.vintage);
+        component.products = [mockProduct];
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(3);
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(5);
+
+      });
+
+      it('should not increase more than 50', () => {
+        const mockProduct = new Product(11, 'Chess', -1, 49, ProductType.vintage);
+        component.products = [mockProduct];
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(50);
+      });
+
+    });
+
+    it('should not change R or Q if product type is immutable', () => {
+      const mockProduct = new Product(11, 'Salt', 1, 80, ProductType.immutable);
+      component.products = [mockProduct];
+
+      component.simulation();
+      expect(component.products[0].quality).toBe(80);
+      expect(component.products[0].sellIn).toBe(1);
+
+      const mockProduct2 = new Product(11, 'Salt', -1, 5, ProductType.immutable);
+      component.products = [mockProduct2];
+      component.simulation();
+      expect(component.products[0].quality).toBe(5);
+      expect(component.products[0].sellIn).toBe(-1);
+    })
+
+    describe('Products with type cured', () => {
+      it('should increased Q by 2 if there are 10 days or less to R', () => {
+        const mockProduct = new Product(11, 'Ham', 9, 10, ProductType.cured);
+        component.products = [mockProduct];
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(12);
+        component.simulation();
+        expect(component.products[0].quality).toBe(14);
+      });
+      it('should increased Q by 3 if there are 5 days or less to R', () => {
+        const mockProduct = new Product(11, 'Ham', 5, 10, ProductType.cured);
+        component.products = [mockProduct];
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(13);
+        component.simulation();
+        expect(component.products[0].quality).toBe(16);
+      });
+      it('should downgrade Q to 0 if there are 0 days or less to R', () => {
+        const mockProduct = new Product(11, 'Ham', 0, 10, ProductType.cured);
+        component.products = [mockProduct];
+
+        component.simulation();
+        expect(component.products[0].quality).toBe(0);
+        component.simulation();
+        expect(component.products[0].quality).toBe(0);
+      });
+    });
 
   });
 });
